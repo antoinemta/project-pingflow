3974605bd753b4fde21bd736a30a5a0b44875c0e
GAVEAU Francois-Axel <gaveau.francois-axel@orange.fr>
2019-01-16 20:42:47 +0100

    Create server.js and create route informationsCity to use API to call 10 monuments by city.

--- back/server.js
+++ back/server.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const request = require('request');
 const passport = require('passport');
 const cors = require('cors');
-const connection = require('./conf');
-const PORT_SERVER = 8080;
+const { PORT_NUMBER, client_id, client_secret } = require('./conf');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(PORT_SERVER, () => {
-  console.log(`Listening on port ${PORT_SERVER}... `);
+app.post('/informationsCity', (req, res) => {
+  let capital = req.body.capital;
+  let infosCity = undefined;
+  request(
+    {
+      url: 'https://api.foursquare.com/v2/venues/explore',
+      method: 'GET',
+      qs: {
+        client_id: `${client_id}`,
+        client_secret: `${client_secret}`,
+        near: `${capital}`,
+        query: 'monuments',
+        v: '20180323',
+        limit: 10
+      }
+    },
+    (err, response, body) => {
+      if (err) {
+        console.error(err);
+      } else {
+        infosCity = JSON.parse(body).response.groups[0].items;
+        res.status(200).send(infosCity);
+      }
+    }
+  );
+});
+
+app.listen(PORT_NUMBER, () => {
+  console.log(`listening on port ${PORT_NUMBER}`);
 });
