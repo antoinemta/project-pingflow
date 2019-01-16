a6a8a31f4fdc8c6333d8bf93509b525723bbe020
Emilie Boeglen <anaporia@protonmail.com>
2019-01-16 19:50:17 +0100

    Add server file with login route and auth wall.

--- back/server.js
+++ back/server.js
@@ -4,12 +4,40 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const passport = require('passport');
 const cors = require('cors');
-const connection = require('./conf');
 const PORT_SERVER = 8080;
+require('./passport-strategy');
+const {
+  PORT_NUMBER,
+  DBurl,
+  Database,
+  saltRounds,
+  connection
+} = require('./conf');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/login', require('./routes/login'));
+
+app.all(
+  '/*',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    next();
+  }
+);
+
+app.use(function(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('you posted:\n');
+  res.end(JSON.stringify(req.body, null, 2));
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(404).send('Not found');
+});
 
 app.listen(PORT_SERVER, () => {
   console.log(`Listening on port ${PORT_SERVER}... `);
