662f16175acba0b0a1d99f1e1f1d0ca8a533a976
GAVEAU Francois-Axel <gaveau.francois-axel@orange.fr>
2019-01-17 01:11:49 +0100

    API call for cities position.

--- back/server.js
+++ back/server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/informationsCities', (req, res) => {
-  let capital = req.body.capital;
-  let infosCity = undefined;
+app.post('/informationsMonuments', (req, res) => {
+  let city = req.body.city;
+  let infosMonuments = undefined;
   request(
     {
       url: 'https://api.foursquare.com/v2/venues/explore',
@@ -23,7 +23,7 @@ app.post('/informationsCities', (req, res) => {
       qs: {
         client_id: `${client_id}`,
         client_secret: `${client_secret}`,
-        near: `${capital}`,
+        near: `${city}`,
         query: 'monuments',
         v: '20180323',
         limit: 10
@@ -33,12 +33,54 @@ app.post('/informationsCities', (req, res) => {
       if (err) {
         console.error(err);
       } else {
-        infosCity = JSON.parse(body).response;
-        console.log(infosCity);
-        res.status(200).send(infosCity);
+        infosMonuments = JSON.parse(body).response.groups[0].items;
+        console.log(infosMonuments);
+        res.status(200).send(infosMonuments);
       }
     }
   );
+});
+
+app.get('/PositionCities', (req, res) => {
+  let PositionCity = [
+    'Paris',
+    'Madrid',
+    'Tokyo',
+    'New-York',
+    'Barcelona',
+    'London',
+    'Moscow',
+    'Rome',
+    'Brussels',
+    'Mexico'
+  ];
+  let latLong = [];
+  PositionCity.map(city => {
+    request(
+      {
+        url: 'https://api.foursquare.com/v2/venues/explore',
+        method: 'GET',
+        qs: {
+          client_id: `${client_id}`,
+          client_secret: `${client_secret}`,
+          near: `${city}`,
+          v: '20180323',
+          limit: 1
+        }
+      },
+      (err, response, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          latLong.push({
+            lat: JSON.parse(body).response.geocode.center.lat,
+            long: JSON.parse(body).response.geocode.center.lng
+          });
+        }
+      }
+    );
+  });
+  res.status(200).send(latLong);
 });
 
 app.listen(PORT_NUMBER, err => {
